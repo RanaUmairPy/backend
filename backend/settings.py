6202efce8a0b1e794428427b7ed120f7c868a770
Umair Saeed <umairsaeed320@gmail.com>
2025-06-16 08:51:26 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -138,12 +138,27 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Neondb',
+        'USER': 'Neondb_owner',
+        'PASSWORD': 'npg_O8yEZRHsW4Qt',
+        'HOST': 'ep-flat-river-a8x5lgti-pooler.eastus2.azure.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
 }
+
 """DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
