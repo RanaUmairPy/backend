d5b644fbba8ce5ace062b12b6ace4600f57301d8
Umair Saeed <umairsaeed320@gmail.com>
2025-06-16 20:35:05 -0700

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import chat_history
+from .views import chat_history,store_player_id
 
 urlpatterns = [
     path('history/<int:user1_id>/<int:user2_id>/', chat_history),
+    path('api/store_player_id/', store_player_id, name='store_player_id'),
 ]
