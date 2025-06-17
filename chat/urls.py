4cd28079e86fc8be3963e0b11a373949cab431c8
Umair Saeed <umairsaeed320@gmail.com>
2025-06-17 09:42:04 -0700

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-from .views import chat_history,store_player_id
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import chat_history,store_player_id,OneSignalViewSet
 
+router = DefaultRouter()
+router.register(r'store_player_id', OneSignalViewSet, basename='store-player-id')
 urlpatterns = [
     path('history/<int:user1_id>/<int:user2_id>/', chat_history),
-    path('api/store_player_id/', store_player_id, name='store_player_id'),
+    path('api/', include(router.urls)),
 ]
