22b519c2263930d9d808159207ab0a05bc1b4523
Umair Saeed <umairsaeed320@gmail.com>
2025-06-16 20:32:31 -0700

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -24,3 +24,10 @@ class Message(models.Model):
             "filename": self.filename,
             "timestamp": self.timestamp.isoformat(),
         }
+        
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    player_id = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.player_id}'
