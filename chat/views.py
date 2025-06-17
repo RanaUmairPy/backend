845ac39947f2987b66cb88d074b0947b991a0f75
Umair Saeed <umairsaeed320@gmail.com>
2025-06-17 09:39:37 -0700

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Message
 from .serializer import MessageSerializer
 from .models import Player
-
+from rest_framework import viewsets
+from .models import OneSignal
+from .serializers import OneSignalSerializer
 
 @api_view(['GET'])
 def chat_history(request, user1_id, user2_id):
@@ -25,3 +27,14 @@ def store_player_id(request):
 
     Player.objects.update_or_create(user=user, defaults={"player_id": player_id})
     return Response({"status": "Player ID saved"})
+
+
+class OneSignalViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request):
+        serializer = OneSignalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Player ID stored"}, status=201)
+        return Response(serializer.errors, status=400)
