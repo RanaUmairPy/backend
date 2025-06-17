8ac078ed525cf67b37ee9cdaa0a4ba7c0776fbae
Umair Saeed <umairsaeed320@gmail.com>
2025-06-17 09:37:32 -0700

    Update serializer.py

--- chat/serializer.py
+++ chat/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Message
-
+from .models import OneSignal
 class MessageSerializer(serializers.ModelSerializer):
     sender_id = serializers.IntegerField(source='sender.id')
     receiver_id = serializers.IntegerField(source='receiver.id')
@@ -9,3 +9,27 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ['id', 'sender_id', 'receiver_id', 'message', 'file', 'filename', 'timestamp']
+
+
+class OneSignalSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = OneSignal
+        fields = ['user_id', 'player_id']
+
+    def validate_user_id(self, value):
+        try:
+            User.objects.get(id=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User does not exist.")
+        return value
+
+    def create(self, validated_data):
+        user_id = validated_data.pop('user_id')
+        user = User.objects.get(id=user_id)
+        onesignal, created = OneSignal.objects.update_or_create(
+            user=user,
+            defaults={'player_id': validated_data['player_id']}
+        )
+        return onesignal
