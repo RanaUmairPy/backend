723cd00ed03ddeb34e99a703b3521e3cf5584627
Umair Saeed <umairsaeed320@gmail.com>
2025-06-20 22:48:00 -0700

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,13 @@
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Message
-from .serializer import MessageSerializer
-from .models import Player
+from .models import Message, OneSignal
+from .serializer import MessageSerializer, OneSignalSerializer
 from rest_framework import viewsets
-from .models import OneSignal
-from .serializer import OneSignalSerializer
+from .services import TokenService
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def chat_history(request, user1_id, user2_id):
     messages = Message.objects.filter(
         sender_id__in=[user1_id, user2_id],
@@ -21,17 +20,22 @@ def chat_history(request, user1_id, user2_id):
 def store_player_id(request):
     user = request.user
     player_id = request.data.get('player_id')
-
     if not player_id:
         return Response({"error": "player_id is required"}, status=400)
-
-    Player.objects.update_or_create(user=user, defaults={"player_id": player_id})
+    OneSignal.objects.update_or_create(user=user, defaults={"player_id": player_id})
     return Response({"status": "Player ID saved"})
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def get_hms_token(request):
+    user = request.user
+    room_id = '68563d93a5ba8326e6eb2dee'  # Your provided Room ID
+    token_service = TokenService()
+    token = token_service.generate_token(user.id, room_id, role="speaker")
+    return Response({"token": token})
 
 class OneSignalViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
-
     def create(self, request):
         serializer = OneSignalSerializer(data=request.data)
         if serializer.is_valid():
